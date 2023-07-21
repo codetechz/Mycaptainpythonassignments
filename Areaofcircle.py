81d16e39873025b5bd06ff09561b3ca2203c3596
codetechz <140146022+codetechz@users.noreply.github.com>
2023-07-21 19:45:32 +0530

    Add files via upload

---
+++ Areaofcircle.py
@@ -0,0 +1,4 @@
+print("Input the radius of the circle:")
+r=float(input())
+Area=3.14*r*r
+print("The area of the circle is:",Area)
---
+++ filextension.py
@@ -0,0 +1,5 @@
+print("Input the Filename:")
+filename=input()
+ext=filename.split(".")
+print("Extension of the file is :",ext[-1])
+
